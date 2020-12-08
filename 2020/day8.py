bc162259b83c1a6fa1a251dd5ce66e6828814839
Sam Murphy <sam-murphy@hotmail.co.uk>
2020-12-08 21:18:06 +0000

    Neatening things up a bit

--- 2020/day8.py
+++ 2020/day8.py
@@ -6,9 +6,8 @@ def parse_file(file_name):
 
 
 def execute(program, fix_loop):
-    jump_instructions = set()
-    nop_instructions = set()
-    last_instruction_changed = -1
+    nops_and_jmps = set()
+    last_instruction_changed = None
     first_time = True
 
     while True:
@@ -26,14 +25,14 @@ def execute(program, fix_loop):
             # Handle instructions
             if program[pc][0] == 'nop':
                 if first_time:
-                    nop_instructions.add(pc)
+                    nops_and_jmps.add(pc)
                 pc += 1
             elif program[pc][0] == 'acc':
                 acc += program[pc][1]
                 pc += 1
             elif program[pc][0] == 'jmp':
                 if first_time:
-                    jump_instructions.add(pc)
+                    nops_and_jmps.add(pc)
                 pc += program[pc][1]
 
         first_time = False
@@ -41,16 +40,13 @@ def execute(program, fix_loop):
         if fix_loop:
             if infinite_loop:
                 # Reset the last instruction we changed
-                if last_instruction_changed != -1:
+                if last_instruction_changed != None:
                     program[last_instruction_changed][0] = 'jmp' if program[last_instruction_changed][0] == 'nop' else 'nop'
                 if infinite_loop:
                     # Change each of the jump/noop instructions one at a time until the inf loop is fixed
-                    if len(jump_instructions):
-                        last_instruction_changed = jump_instructions.pop()
-                        program[last_instruction_changed][0] = 'nop'
-                    elif len(nop_instructions):
-                        last_instruction_changed = nop_instructions.pop()
-                        program[last_instruction_changed][0] = 'jmp'
+                    if len(nops_and_jmps):
+                        last_instruction_changed = nops_and_jmps.pop()
+                        program[last_instruction_changed][0] = 'jmp' if program[last_instruction_changed][0] == 'nop' else 'nop'
 
         if not fix_loop or not infinite_loop:
             break
@@ -81,4 +77,4 @@ print(f'Part 2: TEST: Value of fixed program: {test}')
 
 file_name = "input_day8.txt"
 print(f'Part 1: Value before infinite loop: {solve_part1(file_name)}')
-print(f'Part 2: TEST: Value of fixed program: {solve_part2(file_name)}')
+print(f'Part 2: Value of fixed program: {solve_part2(file_name)}')
